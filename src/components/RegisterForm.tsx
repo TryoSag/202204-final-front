d0f4b890582a113b1ae2d1b0f34128f8bfce7766
AlbSag <92945087+TryoSag@users.noreply.github.com>
2022-05-31 13:31:25 +0200

    added register form basic structure

---
+++ src/components/RegisterForm.tsx
@@ -0,0 +1,25 @@
+const RegisterForm = (): JSX.Element => {
+  return (
+    <form className="register-form" autoComplete="off" noValidate>
+      <div>
+        <label htmlFor="name">
+          Name
+          <input type="text" id="name" placeholder="Name" />
+        </label>
+        <label htmlFor="username">
+          Username
+          <input type="text" id="username" placeholder="Username" />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input type="password" id="password" placeholder="Password" />
+        </label>
+        <button type="submit">Register</button>
+        <p>Do you have an account?</p>
+        <button>Create </button>
+      </div>
+    </form>
+  );
+};
+
+export default RegisterForm;
