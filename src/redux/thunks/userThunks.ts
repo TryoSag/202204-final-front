6b50209a420fac5278ad1d870cd818e20184cabe
AlbSag <92945087+TryoSag@users.noreply.github.com>
2022-05-31 19:24:07 +0200

    added login thunk

--- src/redux/thunks/userThunks.ts
+++ src/redux/thunks/userThunks.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
-import { UserToLogin, UserToRegister } from "../../types/types";
+import jwtDecode from "jwt-decode";
+import { UserData, UserToLogin, UserToRegister } from "../../types/types";
+import { loginActionCreator } from "../features/userSlice";
 import { AppDispatch } from "../store/store";
 
 export const registerThunk =
   (userData: UserToRegister) => async (dispatch: AppDispatch) => {
-    const url: string = `${process.env.REACT_APP_API_URL}user/register`;
+    const url: string = `${process.env.REACT_APP_API_URL}/user/register`;
 
     const { data } = await axios.post<UserToRegister>(url, userData);
     if (data) {
@@ -14,6 +16,19 @@ export const registerThunk =
       };
       dispatch(loginThunk(newUser));
     }
+  };
 
-    dispatch(data);
+export const loginThunk =
+  (userData: UserToLogin) => async (dispatch: AppDispatch) => {
+    const url = `${process.env.REACT_APP_API_URL}/user/login`;
+
+    const {
+      data: { token },
+    } = await axios.post(url, userData);
+
+    if (token) {
+      localStorage.setItem("token", token);
+      const { username, adminUser } = jwtDecode<UserData>(token);
+      dispatch(loginActionCreator({ username, adminUser }));
+    }
   };
