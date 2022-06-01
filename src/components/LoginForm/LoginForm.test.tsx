d6c5278026e6c7827737fe62030e97e2d43b9f2e
AlbSag <92945087+TryoSag@users.noreply.github.com>
2022-06-01 19:23:28 +0200

    fixed login form test

--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -1,28 +1,46 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import store from "../../redux/store/store";
 import LoginForm from "./LoginForm";
 
 describe("Given the LoginForm component", () => {
   describe("When it's called", () => {
     test("Then it should render 1 label with text Username", () => {
-      render(<LoginForm />);
+      render(
+        <Provider store={store}>
+          <LoginForm />
+        </Provider>
+      );
 
       expect(screen.getByLabelText("Username")).toBeInTheDocument();
     });
 
     test("Then it should render 1 label with text Password", () => {
-      render(<LoginForm />);
+      render(
+        <Provider store={store}>
+          <LoginForm />
+        </Provider>
+      );
 
       expect(screen.getByLabelText("Password")).toBeInTheDocument();
     });
 
     test("Then it should render 1 button with text 'Login'", () => {
-      render(<LoginForm />);
+      render(
+        <Provider store={store}>
+          <LoginForm />
+        </Provider>
+      );
 
       expect(screen.getByRole("button", { name: "Login" })).toBeInTheDocument();
     });
 
     test("Then it should render 1 button with text 'to register'", () => {
-      render(<LoginForm />);
+      render(
+        <Provider store={store}>
+          <LoginForm />
+        </Provider>
+      );
 
       expect(
         screen.getByRole("button", { name: "to register" })
