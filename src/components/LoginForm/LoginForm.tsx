61e9d138d330e79e7bcba8e535649f67c97bc3f9
AlbSag <92945087+TryoSag@users.noreply.github.com>
2022-06-01 17:53:26 +0200

    added function to update the inputs data

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,14 +1,43 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../redux/store/store";
+
 const LoginForm = (): JSX.Element => {
+  const dispatch: AppDispatch = useDispatch();
+
+  const emptyForm = {
+    username: "",
+    password: "",
+  };
+  const [formData, setFormData] = useState(emptyForm);
+
+  const updateForm = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setFormData({
+      ...formData,
+      [event.target.id]: event.target.value,
+    });
+  };
+
   return (
     <form className="login-form" autoComplete="off" noValidate>
       <div>
         <label htmlFor="username">
           Username
-          <input type="text" id="username" placeholder="Username" />
+          <input
+            type="text"
+            id="username"
+            placeholder="Username"
+            onChange={updateForm}
+          />
         </label>
         <label htmlFor="password">
           Password
-          <input type="password" id="password" placeholder="Password" />
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            onChange={updateForm}
+          />
         </label>
         <button type="submit">Login </button>
       </div>
