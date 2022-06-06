3ec69045adca334804372401d36be262ce74a784
AlbSag <92945087+TryoSag@users.noreply.github.com>
2022-06-06 11:23:57 +0200

    refactor of list renderizer and added test on list pets component

--- src/components/ListPets/ListPets.tsx
+++ src/components/ListPets/ListPets.tsx
@@ -35,21 +35,37 @@ const ListPets = (): JSX.Element => {
     <main>
       <ul className="container-filter">
         {filterButtons.map(({ text, image, action }) => (
-          <FilterButton text={text} image={image} action={action} />
+          <FilterButton
+            key={`filter ${text}`}
+            text={text}
+            image={image}
+            action={action}
+          />
         ))}
       </ul>
       <ul>
-        {pets.map(({ name, picture, sex, animal }) => {
-          if (animalFilter.includes(animal)) {
-            return (
-              <Pet name={name} picture={picture} sex={sex} animal={animal} />
-            );
-          }
-        })}
+        {pets.map(
+          ({ name, picture, sex, animal, id }) =>
+            animalFilter.includes(animal) && (
+              <Pet
+                key={id}
+                name={name}
+                picture={picture}
+                sex={sex}
+                animal={animal}
+              />
+            )
+        )}
       </ul>
       <section className="container-pagination">
-        <button className="button--previous-page" onClick={previousPage} />
-        <button className="button--next-page" onClick={nextPage} />
+        <label>
+          <button className="button--previous-page" onClick={previousPage} />
+          previous page button
+        </label>
+        <label>
+          <button className="button--next-page" onClick={nextPage} />
+          next page button
+        </label>
       </section>
     </main>
   );
