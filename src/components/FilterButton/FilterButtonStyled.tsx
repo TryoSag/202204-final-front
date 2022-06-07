f2b665befbd4419400d9819de43ea016242ce606
AlbSag <92945087+TryoSag@users.noreply.github.com>
2022-06-07 16:20:24 +0200

    added more style on button filter component

--- src/components/FilterButton/FilterButtonStyled.tsx
+++ src/components/FilterButton/FilterButtonStyled.tsx
@@ -1,20 +1,24 @@
 import styled from "styled-components";
 
 const FilterButtonStyled = styled.li`
+  box-sizing: border-box;
   display: flex;
   flex-direction: column;
+  justify-content: center;
 
-  margin: 10px;
+  padding: 10px;
 
   > img {
     width: 40px;
     height: 40px;
 
     background-color: #ffb800;
+
     border-radius: 10px;
   }
   > span {
     font-size: 20px;
+    text-align: center;
   }
 `;
 
