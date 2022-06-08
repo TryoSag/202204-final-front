44acc85dd72bd7c98ab9c3e2cdb7e1ca7fefa766
Albert <albert.sagol@gmail.com>
2022-06-09 00:09:07 +0200

    added createPetActionCreator on pets slice

--- src/redux/features/petsSlice.ts
+++ src/redux/features/petsSlice.ts
@@ -12,12 +12,17 @@ const petsSlice = createSlice({
     ],
     deletePet: (pets, action: PayloadAction<string>): IPet[] =>
       pets.filter((pet: IPet) => pet.id !== action.payload),
+    createPet: (pets, action: PayloadAction<IPet>): IPet[] => [
+      ...pets,
+      action.payload,
+    ],
   },
 });
 
 export const {
   getPets: getPetsActionCreator,
   deletePet: deletePetActionCreator,
+  createPet: createPetActionCreator,
 } = petsSlice.actions;
 
 export default petsSlice.reducer;
