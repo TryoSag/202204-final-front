870fb50313a1687813ac9729d0ca88bff81c3298
AlbSag <92945087+TryoSag@users.noreply.github.com>
2022-06-08 15:33:01 +0200

    added 1 est on deletePetActionCreator on pet slice

--- src/redux/features/petsSlice.test.ts
+++ src/redux/features/petsSlice.test.ts
@@ -1,5 +1,8 @@
 import { listOfPets } from "../../mocks/mockPets";
-import petsReducer, { getPetsActionCreator } from "./petsSlice";
+import petsReducer, {
+  deletePetActionCreator,
+  getPetsActionCreator,
+} from "./petsSlice";
 
 describe("Given the getPetsActionCreator function", () => {
   describe("When it receives a list of pets", () => {
@@ -8,6 +11,20 @@ describe("Given the getPetsActionCreator function", () => {
       const returnedListOfPets = petsReducer(listOfPets, action);
 
       expect(returnedListOfPets).toEqual(listOfPets);
+    });
+  });
+});
+
+describe("Given the deletePetActionCreator function", () => {
+  describe("When it receives an id of a pet", () => {
+    test("Then it should return the list of pets without the pet with the same id", () => {
+      const petToDeleteId = listOfPets[0].id;
+      const expectedListLength = listOfPets.length - 1;
+
+      const action = deletePetActionCreator(petToDeleteId);
+      const returnedListOfPets = petsReducer(listOfPets, action);
+
+      expect(returnedListOfPets).toHaveLength(expectedListLength);
     });
   });
 });
