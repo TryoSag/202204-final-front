65ccca90e261470e4d8f1fd8a5c16cd767355ce3
AlbSag <92945087+TryoSag@users.noreply.github.com>
2022-06-08 13:09:08 +0200

    added get pets call on list page

--- src/pages/ListPage/ListPage.tsx
+++ src/pages/ListPage/ListPage.tsx
@@ -1,10 +1,20 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Header from "../../components/Header/Header";
 import ListPets from "../../components/ListPets/ListPets";
 import NavigationBar from "../../components/NavigationBar/NavigationBar";
-import { RootState } from "../../redux/store/store";
+import { AppDispatch, RootState } from "../../redux/store/store";
+import { getPetsThunk } from "../../redux/thunks/petsThunks";
 
 const ListPage = (): JSX.Element => {
+  const dispatch: AppDispatch = useDispatch();
+  const token: string | null = localStorage.getItem("token");
+
+  useEffect(() => {
+    if (token) {
+      dispatch(getPetsThunk(token));
+    }
+  }, [dispatch, token]);
   const { adminUser } = useSelector((state: RootState) => state.user);
 
   return (
