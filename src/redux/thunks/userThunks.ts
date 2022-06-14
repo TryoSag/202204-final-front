aabb25cb0f612daf91d60ce2dc3961e8f4cb8497
AlbSag <92945087+TryoSag@users.noreply.github.com>
2022-06-15 01:03:06 +0200

    added dismis toast on thunks

--- src/redux/thunks/userThunks.ts
+++ src/redux/thunks/userThunks.ts
@@ -8,27 +8,28 @@ import { AppDispatch } from "../store/store";
 export const registerThunk =
   (newUserData: UserToRegister) => async (dispatch: AppDispatch) => {
     const url = `${process.env.REACT_APP_API_URL}/user/register`;
-    try {
-      const idLoading = toast.loading("Loading...", { isLoading: true });
+    const idLoading = toast.loading("Loading...", { isLoading: true });
 
+    try {
       const { status, data } = await axios.post<UserToRegister>(url, {
         ...newUserData,
         adminUser: false,
       });
-
-      toast.dismiss(idLoading);
 
       if (status === 201) {
         const userData: UserToLogin = {
           username: data.username,
           password: newUserData.password,
         };
+        toast.dismiss(idLoading);
 
         toast.success(`${data.username} registered`);
 
         dispatch(loginThunk(userData));
       }
     } catch {
+      toast.dismiss(idLoading);
+
       toast.error("Error on register");
     }
   };
@@ -36,26 +37,28 @@ export const registerThunk =
 export const loginThunk =
   (userData: UserToLogin) => async (dispatch: AppDispatch) => {
     const url = `${process.env.REACT_APP_API_URL}/user/login`;
-    try {
-      const idLoading = toast.loading("Loading...", { isLoading: true });
+    const idLoading = toast.loading("Loading...", { isLoading: true });
 
+    try {
       const {
         status,
         data: { token },
       } = await axios.post(url, userData);
-
-      toast.dismiss(idLoading);
 
       if (status === 200) {
         localStorage.setItem("token", token);
 
         const { username, adminUser, eMail } = jwtDecode<User>(token);
 
+        toast.dismiss(idLoading);
+
         toast.success(` Welcome ${username}`);
 
         dispatch(loginActionCreator({ username, adminUser, eMail }));
       }
     } catch {
+      toast.dismiss(idLoading);
+
       toast.error("Error on login");
     }
   };
